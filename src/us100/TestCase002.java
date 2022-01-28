7bb47ec4604836d6d0d3d25c2c6e43168ace5287
Mehmet <fgesen@gmail.com>
2022-01-27 22:24:59 -0500

    // step 6 is done

--- src/us100/TestCase002.java
+++ src/us100/TestCase002.java
@@ -9,4 +9,8 @@ public class TestCase002 {
     // step 5 is done
     // step 6 is done
 
+    //git rm --cached git_practice2.iml// to remove keeping track of folders ".iml"
+    //git rm -r --cached .idea// to remove keeping track of folders ".idea"
+    //git rm -r --cached out // to remove keeping track of folders "out"
+
 }
